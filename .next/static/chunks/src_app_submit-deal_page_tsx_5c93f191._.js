39f1dd8440376789803dd64c00e029feeca81bef
Isha patel <ishuup1205@gmail.com>
2025-07-25 15:52:20 +0530

    Add .next/static/chunks/src_app_submit-deal_page_tsx_5c93f191._.js 1753438937728

---
+++ .next/static/chunks/src_app_submit-deal_page_tsx_5c93f191._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e04c5fa3._.js",
+  "static/chunks/src_8c6f54a5._.js"
+],
+    source: "dynamic"
+});
