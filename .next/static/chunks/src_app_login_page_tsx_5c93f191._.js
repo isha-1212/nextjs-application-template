07f61e1192dd1ac746bd88f24605f3bd069acc28
Isha patel <ishuup1205@gmail.com>
2025-07-25 15:52:39 +0530

    Add .next/static/chunks/src_app_login_page_tsx_5c93f191._.js 1753438956346

---
+++ .next/static/chunks/src_app_login_page_tsx_5c93f191._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_b75440e9._.js",
+  "static/chunks/src_2a08a848._.js"
+],
+    source: "dynamic"
+});
