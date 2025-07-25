0e09588688c199df358d86a413ac24c041cfe408
Isha patel <ishuup1205@gmail.com>
2025-07-25 15:52:24 +0530

    Add .next/static/chunks/src_app_register_page_tsx_5c93f191._.js 1753438941251

---
+++ .next/static/chunks/src_app_register_page_tsx_5c93f191._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_8de011b2._.js",
+  "static/chunks/src_e27122f1._.js"
+],
+    source: "dynamic"
+});
